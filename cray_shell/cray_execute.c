4b68dcec1b70741511947b3643c6053130168948
francis <frankchomba10@gmail.com>
2023-10-17 21:49:57 +0000

    correction betty

--- cray_shell/cray_execute.c
+++ cray_shell/cray_execute.c
@@ -9,16 +9,6 @@ void run_command(const char *command)
 
 	child_pid = fork();
 
-<<<<<<< HEAD
-    if (child_pid == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (child_pid == 0) {
-        /*Child process*/
-        /*Construct the full path to the command*/
-        char command_path[256];
-        snprintf(command_path, sizeof(command_path), "/bin/%s", command);
-=======
 	if (child_pid == -1)
 	{
 		perror("fork");
@@ -30,18 +20,9 @@ void run_command(const char *command)
 		/* Construct the full path to the command */
 		char command_path[256];
 		snprintf(command_path, sizeof(command_path), "/bin/%s", command);
->>>>>>> 35a082f5abe7b4b03aa34ae1f3cfeb8ccbd18a1a
 
 		execve(command_path, args, NULL);
 
-<<<<<<< HEAD
-        perror("execve");
-        exit(EXIT_FAILURE);
-    } else {
-        /*Parent process*/
-        wait(NULL);
-    }
-=======
 		perror("execve");
 		exit(EXIT_FAILURE);
 	}
@@ -50,6 +31,4 @@ void run_command(const char *command)
 		/* Parent process */
 		wait(NULL);
 	}
->>>>>>> 35a082f5abe7b4b03aa34ae1f3cfeb8ccbd18a1a
 }
-
