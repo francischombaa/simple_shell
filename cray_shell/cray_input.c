241fffec95e42a45efccdcc62650bf07a168e090
francis <frankchomba10@gmail.com>
2023-10-17 21:29:40 +0000

    correction betty

--- cray_shell/cray_input.c
+++ cray_shell/cray_input.c
@@ -10,14 +10,14 @@
  * provided buffer, removing the trailing newline character if present.
  */
 void cray_input(char *command, size_t size) {
-    if (fgets(command, size, stdin) == NULL) {
-        if (feof(stdin)) {
-            cray_print("\n");
-            exit(EXIT_SUCCESS);
-        } else {
-            perror("fgets");
-            exit(EXIT_FAILURE);
-        }
-    }
-    command[strcspn(command, "\n")] = '\0'; /* Remove newline */
+	if (fgets(command, size, stdin) == NULL) {
+		if (feof(stdin)) {
+			cray_print("\n");
+			exit(EXIT_SUCCESS);
+		} else {
+			perror("fgets");
+			exit(EXIT_FAILURE);
+		}
+	}
+	command[strcspn(command, "\n")] = '\0'; /* Remove newline */
 }
