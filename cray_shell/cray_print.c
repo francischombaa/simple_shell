aff34f680265d543fbe05be5eebb1d5d641f077d
Ketere <keterel8@gmail.com>
2023-10-17 15:29:17 -0700

    simple

--- cray_shell/cray_print.c
+++ cray_shell/cray_print.c
@@ -1,8 +1,13 @@
 #include "cray_shell.h"
-/*
-*Author: Fraanklin Chombaa & Leonard Ketere
-*This is a custom print function which will be used insted of printf
-*/
-void cray_print(const char *crayprint) {
-    write(STDOUT_FILENO, crayprint, strlen(crayprint));
+
+/**
+ * cray_print - Custom print function used instead of printf.
+ * @crayprint: The string to be printed.
+ *
+ * Description: This function writes the specified string to the standard output.
+ */
+void cray_print(const char *crayprint)
+{
+        write(STDOUT_FILENO, crayprint, strlen(crayprint));
 }
+
