08b9c04ce41bc40e98746d3dd343ee897e76ebe5
francis <frankchomba10@gmail.com>
2023-10-17 21:34:03 +0000

    correction betty

--- cray_shell/cray_input.c
+++ cray_shell/cray_input.c
@@ -12,13 +12,13 @@
 void cray_input(char *command, size_t size)
 {
 	if (fgets(command, size, stdin) == NULL)
-	  {
+	{
 		if (feof(stdin))
-		  {
+		{
 			cray_print("\n");
 			exit(EXIT_SUCCESS);
 		} else
-		  {
+		{
 			perror("fgets");
 			exit(EXIT_FAILURE);
 		}
